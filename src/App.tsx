1c70228933ebb3c56ad836e725f1b7c9d381e730
DESKTOP-AG67U87\User <second3965@gmail.com>
2022-12-11 21:21:46 +0900

    setting eslint pretter

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 
 function App() {
-  return (
-    <div className="App">
-   
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
